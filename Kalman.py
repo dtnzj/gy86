0a96e9f674cec6edde70bf5120cae659f64883ac
dtnzj <dt_nanzhijie@163.com>
2018-06-17 00:16:57 +0800

    add a plot test

--- Kalman.py
+++ Kalman.py
@@ -1,20 +1,53 @@
+#!/usr/bin/env python
+
 import numpy as np
+import math
+import matplotlib.pyplot as plt
+
 from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
+import time 
+import numpy as np
 
-def get_some_measurement():
+class static_var():
+    y_static = 0;
+    ylist = np.array([])
+    def get_some_measurement(self):
+        
+        t = time.clock()
+        y = math.sin( 0.1 * math.pi* t)
+        ydot = y - self.y_static
+        self.y_static = y
+        # return (np.array([y, ydot]).T )
+        return (np.array(y))
     
-    pass
+    def do_something_amazing(self, x):
+        self.ylist = np.hstack((self.ylist, x))
+        
 
-def do_something_amazing(x):
-    pass
+        plt.plot(self.ylist)
+        plt.draw()
+        
 
 
-    
-if __name__ == "__main__":
+def plotTest():
+    k = static_var();
+    plt.ion()
+    plt.figure(1)
+    plt.show()
+    while 1:
+        tmp = k.get_some_measurement()
+        print (tmp)
+        k.do_something_amazing(tmp)
+        plt.pause(0.01)
+
+        
+
+
+def test():
     dt = 0.001
-    
-    
+
+
     my_filter = KalmanFilter(dim_x=2, dim_z=1)
     my_filter.x = np.array([[0.0],
                             [0.0]])       
@@ -41,3 +74,7 @@ if __name__ == "__main__":
         # do something with the output
         x = my_filter.x
         do_something_amazing(x)
+    pass
+
+if __name__ == "__main__":
+    plotTest();
